d186ba6f433765b58439cebbf97413b2c7c0ac8c
PeteJax123 <67374382+PeteJax123@users.noreply.github.com>
2022-07-26 13:35:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    splunk = {
+      source  = "splunk/splunk"
+      version = "1.0.0"
+    }
+  }
+}
+
+provider "splunk" {
+  url                  = "splunk01.corp.local:8089"
+  username             = "user"
+  password             = "password"
+}
+
+resource "splunk_apps_local" "terraform_splunk_app" {
+  filename         = true
+  name             = "/tmp/terraform-cloud-for-splunk_005.tgz"
+  explicit_appname = "terraform_cloud_for_splunk"
+}
